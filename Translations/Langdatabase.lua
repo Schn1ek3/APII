c8fa65b306e74330fc119df23d987b4193bc5775
ONELua Team <oneluaproject@gmail.com>
2020-02-10 07:52:22 -0600

    Add japanese

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -1,5 +1,6 @@
 Online_Langs = {
-{ id =  "ENGLISH_US",	version = "01.00",		author =  "gdljjrod" },
-{ id =  "SPANISH",		version = "01.00",		author =  "gdljjrod" },
-{ id =  "ITALIAN",		version = "01.00",		author =  "TheHeroeGAC" },
+{ id =  "ENGLISH_US",	version = "01.01",		author =  "gdljjrod" },
+{ id =  "SPANISH",		version = "01.01",		author =  "gdljjrod" },
+{ id =  "ITALIAN",		version = "01.01",		author =  "TheHeroGAC" },
+{ id =  "JAPANESE",		version = "01.01",		author =  "Kouchan" },
 }
