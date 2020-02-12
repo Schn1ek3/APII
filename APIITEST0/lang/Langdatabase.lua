39edc6648cf29d20059aa67eb58477d8bcc97bdf
KOZ39 <koz39c@gmail.com>
2020-02-12 19:19:51 +0900

    Uniform white-space

--- APIITEST0/lang/Langdatabase.lua
+++ APIITEST0/lang/Langdatabase.lua
@@ -1,6 +1,6 @@
-Langs = {
-{ id =  "ENGLISH_US",		version = "01.01",		author =  "gdljjrod" },
-{ id =  "ITALIAN",		version = "01.01",		author =  "TheHeroGAC" },
-{ id =  "JAPANESE",		version = "01.01",		author =  "Kouchan" },
-{ id =  "SPANISH",		version = "01.01",		author =  "gdljjrod" },
+Langs =                                                  {
+{ id =                                                  "ENGLISH_US",		version 
+{ id =                                                  "ITALIAN",		version 
+{ id =                                                  "JAPANESE",		version 
+{ id =                                                  "SPANISH",		version 
 }
