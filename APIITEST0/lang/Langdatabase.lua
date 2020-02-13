a23b8c5d546f1d8bceb9f44424d9a289df30dcbe
David Nuñez <devdavisnunez@gmail.com>
2020-02-12 18:31:22 -0600

    Update Langdatabase.lua

--- APIITEST0/lang/Langdatabase.lua
+++ APIITEST0/lang/Langdatabase.lua
@@ -1,6 +1,6 @@
 Online_Langs = {
-{ id =  "ENGLISH_US",	version = "01.01",		author =  "gdljjrod" },
-{ id =  "SPANISH",		version = "01.01",		author =  "gdljjrod" },
-{ id =  "ITALIAN",		version = "01.01",		author =  "TheHeroGAC" },
-{ id =  "JAPANESE",		version = "01.01",		author =  "Kouchan" },
+{ id =  "ENGLISH_US",	version = "01.02",		author =  "gdljjrod" },
+{ id =  "SPANISH",		version = "01.02",		author =  "gdljjrod" },
+{ id =  "ITALIAN",		version = "01.02",		author =  "TheHeroGAC" },
+{ id =  "JAPANESE",		version = "01.02",		author =  "Kouchan" },
 }
